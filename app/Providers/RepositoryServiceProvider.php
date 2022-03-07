7d803a92c184258f9b59454802b91cb73a17c303
= <=>
2022-03-07 14:44:44 -0300

    fix: bind ProductServiceContract for ProductService

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,6 +7,8 @@ use App\Repositories\PaymentMethod\PaymentMethodRepositoryContract;
 use App\Repositories\PaymentMethod\PaymentMethodRepositoryEloquent;
 use App\Repositories\Product\ProductRepositoryContract;
 use App\Repositories\Product\ProductRepositoryEloquent;
+use App\Services\Product\ProductService;
+use App\Services\Product\ProductServiceContract;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,7 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register() {
         $this->app->bind(PaymentMethodRepositoryContract::class, PaymentMethodRepositoryEloquent::class);
         $this->app->bind(ProductRepositoryContract::class, ProductRepositoryEloquent::class);
+        $this->app->bind(ProductServiceContract::class, ProductService::class);
     }
 
 
