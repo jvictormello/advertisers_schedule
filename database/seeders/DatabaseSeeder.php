40ee43ed673017c436d99d80db88668a227f213e
Victor Prestes <victor.prestes@atlastechnol.com>
2022-03-16 16:38:08 -0300

    fix: remove unused files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersSeeder::class,
-            ProductsSeeder::class,
-            PaymentMethodsSeeder::class,
-            TransactionsSeeder::class,
         ]);
     }
 }
