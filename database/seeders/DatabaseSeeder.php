2d431d7953be4176c4ab646956874a177dde0da3
Leonardo Laporte <leonardo.laporte@atlastechnol.com>
2022-05-02 07:41:27 -0300

    feat(CATE-1429): add structure migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call([
+            AdvertisersSeeder::class
+        ]);
+        $this->call([
+            ContractorsSeeder::class
+        ]);
     }
 }
