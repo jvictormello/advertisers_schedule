0eb3d235cb018b121f4f2acc13377418e93c970e
João Victor Morais de Mello <joaovictor.mello@atlastechnol.com>
2023-04-06 18:36:10 -0300

    CATE-3631: Create Advertiser Create Migration, Model, Factory, and Seeder

---
+++ database/factories/AdvertiserFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Advertiser;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdvertiserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Advertiser::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->email,
+            'username' => $this->faker->unique()->userName,
+            'login' => $this->faker->unique()->userName,
+            'password' => $this->faker->password(),
+            'profile_description' => $this->faker->paragraph(),
+        ];
+    }
+}
