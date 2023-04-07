dea30dce9bada8da9cdcee3ddd0277084018c4e1
João Victor Morais de Mello <joaovictor.mello@atlastechnol.com>
2023-04-06 21:55:25 -0300

    CATE-3631: Create Price Create Migration, Model, Factory, and Seeder

---
+++ database/factories/PriceFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Advertiser;
+use App\Models\Price;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PriceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Price::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'advertiser_id' => Advertiser::first()->id,
+            'price_per_hour' => 150.00,
+        ];
+    }
+}
