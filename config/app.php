40355338fc5c2d86641a2223c2a7df47d6ce3c49
João Victor Morais de Mello <joaovictor.mello@atlastechnol.com>
2023-04-10 15:49:49 -0300

    feat: Create Advertiser Rep Service Provider

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Repositories Service Providers
+         */
+        App\Providers\AdvertiserRepositoryServiceProvider::class,
+
     ],
 
     /*
