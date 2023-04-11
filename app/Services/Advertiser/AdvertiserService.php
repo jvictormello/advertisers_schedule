a412f95b2385061d8cafb079bdb5612e324d9251
João Victor Morais de Mello <joaovictor.mello@atlastechnol.com>
2023-04-10 23:11:27 -0300

    feat: Create cached GETs Create cached getAllCachedAdvertisers and cached getCachedAdvertiserById

--- app/Services/Advertiser/AdvertiserService.php
+++ app/Services/Advertiser/AdvertiserService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Advertiser;
 
 use App\Repositories\Advertiser\AdvertiserRepositoryContract;
+use Illuminate\Support\Facades\Cache;
 
 class AdvertiserService implements AdvertiserServiceContract
 {
@@ -18,8 +19,26 @@ class AdvertiserService implements AdvertiserServiceContract
         return $this->advertiserRepository->all()->toArray();
     }
 
+    public function getAllCachedAdvertisers()
+    {
+        return Cache::store('redis')->remember('getAllAdvertisers', 300, function () {
+            return $this->getAllAdvertisers();
+        });
+    }
+
     public function getAdvertiserById(int $advertiserId)
     {
-       return $this->advertiserRepository->getById($advertiserId); 
+        return $this->advertiserRepository->getById($advertiserId); 
+    }
+
+    public function getCachedAdvertiserById(int $advertiserId)
+    {
+        if ($this->getAdvertiserById($advertiserId) != Cache::store('redis')->get('getCachedAdvertiserById'.$advertiserId)) {
+            Cache::store('redis')->forget('getCachedAdvertiserById'.$advertiserId);
+        }
+
+        return Cache::store('redis')->remember('getCachedAdvertiserById'.$advertiserId, 600, function () use ($advertiserId) {
+            return $this->getAdvertiserById($advertiserId);
+        });
     }
 }
--- app/Services/Advertiser/AdvertiserServiceContract.php
+++ app/Services/Advertiser/AdvertiserServiceContract.php
@@ -5,5 +5,7 @@ namespace App\Services\Advertiser;
 interface AdvertiserServiceContract
 {
     public function getAllAdvertisers();
+    public function getAllCachedAdvertisers();
     public function getAdvertiserById(int $advertiserId);
+    public function getCachedAdvertiserById(int $advertiserId);
 }
